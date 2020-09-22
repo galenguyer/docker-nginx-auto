b7be1e2824999f3a1c6c9969ae8f256decd035de
Galen Guyer <galen@galenguyer.com>
2020-09-22 09:17:32 -0400

    Add confs and dockerfile for autoindex version

--- build.sh
+++ build.sh
@@ -32,6 +32,13 @@ docker build \
         -t "$registry"/nginx-simple:latest \
         -f simple.Dockerfile .
 
+docker build \
+        --build-arg NGINX_VER="$nginx_version" \
+        --build-arg CORE_COUNT="$core_count" \
+        -t "$registry"/nginx-autoindex:"$nginx_version" \
+        -t "$registry"/nginx-autoindex:latest \
+        -f autoindex.Dockerfile .
+
 # if a registry is specified, push to it
 if [ "$registry" != "local" ]; then
 	docker push "$registry"/nginx-simple:"$nginx_version"
